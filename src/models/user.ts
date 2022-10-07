293f0fad3e99a577c11ea97a8851c7c19d8a6bdc
Horlakz <officialhorlakz@gmail.com>
2022-10-07 12:47:42 +0100

    creates file for user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,13 +1,25 @@
-import { Sequelize, DataTypes } from "sequelize";
+import bcrypt from "bcryptjs";
+import { DataTypes, Model, Sequelize } from "sequelize";
 
 const sequelize = new Sequelize(`${process.env.DB_URL}`);
 
-const User = sequelize.define(
+interface UserAttributes extends Model {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const User = sequelize.define<UserAttributes>(
   "User",
   {
     id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
+      type: DataTypes.STRING,
+      set() {
+        this.setDataValue("id", Math.random().toString(36).substring(2, 15));
+      },
       primaryKey: true,
     },
     name: {
@@ -17,9 +29,18 @@ const User = sequelize.define(
     email: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
     },
     password: {
       type: DataTypes.STRING,
+      set(value: string) {
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(value, salt);
+        this.setDataValue("password", hash);
+      },
       allowNull: false,
     },
   },
@@ -28,3 +49,5 @@ const User = sequelize.define(
     timestamps: true,
   }
 );
+
+export default User;
