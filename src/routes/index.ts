279f1e9186d7809a39c66b1cb09d944d0f2adedc
Horlakz <officialhorlakz@gmail.com>
2022-10-07 22:45:49 +0100

    updates file with link routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 // app routes
 import mediaRoutes from "./media";
 import userRoutes from "./user";
+import linkRoutes from "./link";
 
 // initialize express router
 const router = Router();
@@ -10,6 +11,7 @@ const router = Router();
 // mount routes
 router.use("/auth", userRoutes);
 router.use("/media", mediaRoutes);
+router.use("/links", linkRoutes);
 
 // export router
 export default router;
