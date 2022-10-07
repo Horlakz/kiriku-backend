1d2af5a10edeb8ca72aa864946145ccf1350b01f
Horlakz <officialhorlakz@gmail.com>
2022-10-07 15:07:18 +0100

    run sequelize migration, seeds and model

---
+++ src/seeders/20221007135517-demo-user.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert("users", [
+      {
+        name: "John Doe",
+        email: "example@example.com",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("users", null, {});
+  },
+};
