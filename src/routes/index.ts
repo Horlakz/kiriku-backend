a026f440aa3f2a3ab2ae88cd35257fada1a61ec3
Horlakz <officialhorlakz@gmail.com>
2022-10-07 15:34:38 +0100

    removes nodejs files

--- src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-const router = Router();
-
-import UserRoutes from "./user";
-
-router.use("/user", UserRoutes);
-
-export default router;
--- src/utils.ts
+++
@@ -1,7 +0,0 @@
-import { Secret, sign } from "jsonwebtoken";
-
-// generate token function
-export const generateToken = (id: string) =>
-  sign({ id }, process.env.JWT_SECRET as Secret, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
