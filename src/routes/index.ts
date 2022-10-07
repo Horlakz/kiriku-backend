e29933c5c8bce8f6905cb5409dd241952e12bede
Horlakz <officialhorlakz@gmail.com>
2022-10-07 12:49:33 +0100

    adds user routes to index route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,8 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello, world!");
-});
+import UserRoutes from "./user";
+
+router.use("/user", UserRoutes);
 
 export default router;
