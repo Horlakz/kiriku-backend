9fd0eda5afb25f71eba0ee042d71be686ee87931
Horlakz <officialhorlakz@gmail.com>
2022-10-07 23:44:56 +0100

    creates file for handling messages middleware

---
+++ src/middlewares/message.ts
@@ -0,0 +1,29 @@
+import type { Request, Response, NextFunction } from "express";
+
+import { protect } from "../middlewares/auth";
+import Link from "../models/link";
+
+export const isPublic = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { link } = req.params;
+
+  try {
+    const getLink = await Link.findOne({ link });
+
+    if (getLink === null) {
+      return res.status(404).json({ message: "Link not found" });
+    }
+
+    if (getLink.isPublic) {
+      next();
+    } else {
+      protect(req, res, next);
+    }
+  } catch (err) {
+    console.log(err);
+    if (err instanceof Error) res.json({ message: err.message });
+  }
+};
