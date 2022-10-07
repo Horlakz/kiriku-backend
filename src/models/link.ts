8628ae726292777d0252bf1c8e800a15500e1e1d
Horlakz <officialhorlakz@gmail.com>
2022-10-07 22:46:42 +0100

    creates file for link data structure

---
+++ src/models/link.ts
@@ -0,0 +1,56 @@
+import { Document, Schema, model, Types } from "mongoose";
+
+interface Link extends Document {
+  user: Types.ObjectId;
+  link: string;
+  description: string;
+  isPublic: boolean;
+  isActive: boolean;
+}
+
+const linkSchema = new Schema<Link>(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      required: [true, "User is required"],
+      ref: "User",
+    },
+    link: {
+      type: String,
+      unique: true,
+    },
+    description: {
+      type: String,
+      maxlength: [100, "Description must not be more than 100 characters"],
+    },
+    isPublic: {
+      type: Boolean,
+      default: false,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+linkSchema.pre("save", async function (next) {
+  if (!this.isModified("link")) {
+    return next();
+  }
+
+  const link = (Math.random() * 1000000).toString();
+  const linkExists = await model<Link>("Link").findOne({ link });
+
+  if (linkExists) {
+    this.link = link;
+    next();
+  }
+
+  this.link = link;
+
+  next();
+});
+
+export default model<Link>("Link", linkSchema);
