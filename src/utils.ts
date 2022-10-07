adfc495b97b9f077311d0666ff75a978e50b472f
Horlakz <officialhorlakz@gmail.com>
2022-10-07 19:08:08 +0100

    Revert "init"
    This reverts commit 931e11457edd50907fbce335728b592ea60c3f3f.

--- src/utils.ts
+++
@@ -1,8 +0,0 @@
-import type { Types } from "mongoose";
-import { sign, Secret } from "jsonwebtoken";
-
-// generate token function
-export const generateToken = (id: Types.ObjectId) =>
-  sign({ id }, process.env.JWT_SECRET as Secret, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
