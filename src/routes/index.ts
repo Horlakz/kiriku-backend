1c17e8bc8bb872f67dede197615a698f1cef82fb
Horlakz <officialhorlakz@gmail.com>
2022-10-07 23:50:20 +0100

    adds messages route to main routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import { Router } from "express";
 import mediaRoutes from "./media";
 import userRoutes from "./user";
 import linkRoutes from "./link";
+import messageRoutes from "./message";
 
 // initialize express router
 const router = Router();
@@ -12,6 +13,7 @@ const router = Router();
 router.use("/auth", userRoutes);
 router.use("/media", mediaRoutes);
 router.use("/links", linkRoutes);
+router.use("/messages", messageRoutes);
 
 // export router
 export default router;
