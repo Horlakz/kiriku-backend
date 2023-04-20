b87513a6c1732493a6c3c2929c6ec3dbda045590
horlakz <officialhorlakz@gmail.com>
2023-04-20 02:56:57 +0100

    feat(src/): add routes for form

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,7 @@ import mediaRoutes from "./media";
 import userRoutes from "./user";
 import linkRoutes from "./link";
 import messageRoutes from "./message";
+import formRoutes from "./form";
 
 // initialize express router
 const router = Router();
@@ -14,6 +15,7 @@ router.use("/auth", userRoutes);
 router.use("/media", mediaRoutes);
 router.use("/links", linkRoutes);
 router.use("/messages", messageRoutes);
+router.use("/forms", formRoutes);
 
 // export router
 export default router;
